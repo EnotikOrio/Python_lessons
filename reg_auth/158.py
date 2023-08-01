3a976c7049754259676b6bf3d3e1dc0c3cd8cd80
EnotikOrio <vanyagmer0@gmail.com>
2023-08-01 15:29:50 +0300

    main

---
+++ reg_auth/158.py
@@ -0,0 +1,10 @@
+def add_user(login, password):
+    with open("users.txt", "a") as file:
+        file.write(f"{login}, {password} \n")
+
+def main():
+    while True:
+          login = (input("Введіть Логін, або натиснить enter щоб вийти: "))
+          if not login:
+               break
+          password = input("Введіть пароль:")
