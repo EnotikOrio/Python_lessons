14022f537dc926dee9b949491b17760f7ed850a8
EnotikOrio <vanyagmer0@gmail.com>
2023-08-09 16:54:54 +0300

    tic-tac-toe

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,11 +1,17 @@
+def show_feald():
+    for i in range(0, len(feald)):
+        for j in range(0, len(feald[i])):
+            print(feald[i][j],end="\t")
+        print()
+
+
 feald = [["*","*","*"],
          ["*","*","*"],
          ["*","*","*"]]
+show_feald()
 
-for i in range(0, len(feald)):
-    for j in range(0, len(feald[i])):
-        print(feald[i][j],end="\t")
+while True:
+    x, y = map(int, input("Введіть коорденати: ").split())
+    feald[x - 1][y - 1] = "X"
     print()
-
-
-print(feald)
+    show_feald()
