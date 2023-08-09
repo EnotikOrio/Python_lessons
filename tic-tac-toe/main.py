d327906e541176382e2991e5e434c907482b1c83
EnotikOrio <vanyagmer0@gmail.com>
2023-08-09 16:35:26 +0300

    tic-tac-toe

---
+++ tic-tac-toe/main.py
@@ -0,0 +1,11 @@
+feald = [["*","*","*"],
+         ["*","*","*"],
+         ["*","*","*"]]
+
+for i in range(0, len(feald)):
+    for j in range(0, len(feald[i])):
+        print(feald[i][j],end="\t")
+    print()
+
+
+print(feald)
