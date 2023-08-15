7a75e502c9607db3a4c7a9049e93c3dac20b08f4
EnotikOrio <vanyagmer0@gmail.com>
2023-08-15 14:56:19 +0300

    oop

--- oop/002.py
+++ oop/002.py
@@ -1,4 +1,11 @@
 
 class Calc:
     def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
         pass
+    def summ(self):
+        return self.n1 + self.n2
+
+res_1 = Calc(5, 7)
+print(res_1.summ())
