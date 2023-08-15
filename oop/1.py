c57ab89b62691da33c2d53fc38ba64bf78f8a7d7
EnotikOrio <vanyagmer0@gmail.com>
2023-08-15 14:45:57 +0300

    oop

--- oop/1.py
+++ oop/1.py
@@ -1,3 +1,30 @@
 #Клас це відносне креслення обьекта(це шаблон для створення обьекта)
 #обьект классу називають екземпляром классу
 # кожен класс може мати в собі основні атрибути та методи
+
+
+class Car:
+    name = "Nisan"
+    model = "Skyline GT-R-R34"
+    year = 2000
+    def start_engine(self):
+        print("Двигун почав працювати: ")
+    def off_engine(self):
+        print("Двигун завершив працю: ")
+    def on_lights(self):
+        print("Фари почали працювати: ")
+    def off_lights(self):
+        print("Фари завершили працю: ")
+    def turn_left(self):
+        print("Колеса повернулись на ліво: ")
+    def turn_right(self):
+        print("Колеса повернулись на право:")
+    
+car1 = Car()
+car2 = Car()
+print(type(car1))
+print(car1.year)
+print(car2.year)
+car1.year = 1875
+print(car1.year)
+print(car2.start_engine(10))
