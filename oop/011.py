410d40db5ea9d97f8433972283cadceeb0f3c4fb
EnotikOrio <vanyagmer0@gmail.com>
2023-08-23 16:40:44 +0300

    oop

--- oop/011.py
+++ oop/011.py
@@ -1,26 +1,37 @@
 import curses
 import time
+import random
+
+cord_x = []
+cord_y = []
+
 #Зробити другу зірку 
 class Anime:
     def __init__(self, window):
         self.window = window
-        self.x_right = 0
-        self.x_left = self.window.getmaxyx()[1] - 1
+        self.x = 0
         self.y = 0
         
         
     def animate(self):
-        while True:
-            self.window.clear()
+        for i in range(1, 500):
+            #self.window.clear()
             """for i in range(2):
                 self.window.addch(self.y, (self.x + i) % self.window.getmaxyx()[1], '*')"""
-            self.window.addch(self.y, self.x_left, '*')
-            self.window.addch(self.y, self.x_right, '*')
+            r1 = random.randint(0, 5)
+            self.window.addch(self.y, self.x, '*')
+            cord_x.append(self.x)
+            cord_y.append(self.y)
+            for j in range(0, r1):
+                self.window.addch(self.y, self.x, ' ')
             self.window.refresh()
-            time.sleep(0.1)
-            self.x_left = (self.x_left - 1) % self.window.getmaxyx()[1]
-            self.x_right = (self.x_right + 1) % self.window.getmaxyx()[0]
-            self.y = (self.y + 1) % self.window.getmaxyx()[0]
+            time.sleep(0.001)
+            self.x = (self.x + 1) % self.window.getmaxyx()[1]
+            if i % 150 == 0:
+                self.x = 0
+                self.y = (self.y + 1) % self.window.getmaxyx()[0]
+
+            
 
 
 def main(stdscr):
@@ -29,3 +40,6 @@ def main(stdscr):
     animation = Anime(stdscr)
     animation.animate()
 curses.wrapper(main)
+print(cord_x)
+print()
+print(cord_y)
